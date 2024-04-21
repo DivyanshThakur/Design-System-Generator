e77f0ad2fb76946bac340abc12f664812238be63
Divyansh Singh Thakur <divyansh2140@gmail.com>
2024-04-22 00:21:56 +0530

    Added redux slice

---
+++ client/src/redux/slices/selectedProject.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    _id: '',
+    name: '',
+};
+
+const selectedProjectSlice = createSlice({
+    name: 'projectData',
+    initialState,
+    reducers: {
+        setProjectData: (state, action) => {
+            console.log(action)
+            const payload = action.payload;
+            if (payload._id) state._id = payload._id;
+            if (payload.name) state.name = payload.name;
+        },
+        resetProjectData: (state) => {
+            state._id = '';
+            state.name = '';
+        },
+    },
+});
+
+export const { setProjectData, resetProjectData } =
+    selectedProjectSlice.actions;
+export default selectedProjectSlice.reducer;
