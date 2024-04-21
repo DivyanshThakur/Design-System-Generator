c2425766c5832f7e28f29b5ac2c52c17aa877292
Divyansh Singh Thakur <divyansh2140@gmail.com>
2024-04-22 00:55:18 +0530

    added body parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -16,7 +17,8 @@ const app = express();
 connectDB();
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(
@@ -29,7 +31,7 @@ app.use(
 app.use('/api', appRouter);
 
 app.get('/', (_req: Request, res: Response) => {
-    res.json({ message: 'Namaste From Covrzy API' });
+    res.json({ message: 'Namaste From Identity API' });
 });
 
 app.listen(generalConfig.PORT, () =>
