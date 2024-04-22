c2bf249d45b3eee0dd85dac1fe9b9114ed5a76c3
Divyansh Singh Thakur <divyansh2140@gmail.com>
2024-04-22 20:16:18 +0530

    Added required html elements for user to edit and view

---
+++ client/src/components/StyledComponentContainer.tsx
@@ -0,0 +1,318 @@
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    Container,
+    Grid,
+    MenuItem,
+    Select,
+    Typography,
+} from '@mui/material';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Item } from './InputList';
+import StyledComponentViewer from './StyledComponentViewer';
+import {
+    ComponentInitialStateType,
+    setComponents,
+    setSelectedComponentByType,
+    setStyle,
+} from '../redux/slices/component';
+import {
+    useGetAllComponentsQuery,
+    useUpdateComponentByProjectIdMutation,
+} from '../redux/api/component';
+
+const StyledComponentContainer = () => {
+    const dispatch = useDispatch();
+    const projectData = useSelector((state: any) => state.selectedProject);
+    const [updateComponent] = useUpdateComponentByProjectIdMutation();
+
+    const { data } = useGetAllComponentsQuery(projectData._id);
+    const theme = useSelector((state: any) => state.theme);
+    const { selectedComponent }: ComponentInitialStateType = useSelector(
+        (state: any) => state.component,
+    );
+
+    const componentsTypes = [
+        'button',
+        'input-text',
+        'radio',
+        'checkbox',
+        'select',
+    ];
+
+    useEffect(() => {
+        if (data) {
+            dispatch(setComponents(data));
+            if (selectedComponent._id === '')
+                dispatch(setSelectedComponentByType('button'));
+        }
+    }, [data, dispatch, selectedComponent]);
+
+    const handleTypeButtonClick = async (type: string) => {
+        await updateComponent({
+            projectId: projectData._id,
+            componentId: selectedComponent._id,
+            ...selectedComponent,
+        });
+        dispatch(setSelectedComponentByType(type));
+    };
+
+    const handleChange = (e: any) => {
+        dispatch(setStyle({ [e.target.name]: e.target.value }));
+    };
+
+    return (
+        <Container
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                paddingTop: 20,
+                paddingBottom: 20,
+            }}
+        >
+            <ButtonGroup style={{ alignSelf: 'center', marginBottom: 40 }}>
+                {componentsTypes.map((type) => (
+                    <Button
+                        key={`btn-${type}`}
+                        variant={
+                            selectedComponent.type === type
+                                ? 'contained'
+                                : 'outlined'
+                        }
+                        onClick={() => handleTypeButtonClick(type)}
+                    >
+                        {type}
+                    </Button>
+                ))}
+            </ButtonGroup>
+            <Grid container display="flex" justifyContent="center" gap={2}>
+                <Grid item sm={5}>
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        border="1px solid black"
+                        borderRadius={4}
+                        padding={2}
+                        gap={2}
+                    >
+                        <Box>
+                            <Typography
+                                component="div"
+                                display="inline-block"
+                                width={150}
+                            >
+                                Text Color:
+                            </Typography>
+                            {
+                                <Select
+                                    style={{
+                                        paddingLeft: 10,
+                                        marginLeft: 20,
+                                        minWidth: 150,
+                                    }}
+                                    variant="filled"
+                                    id="select-color"
+                                    value={selectedComponent.styles.textColor}
+                                    label="Text Color"
+                                    name="textColor"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value="">None</MenuItem>
+                                    {theme.colors?.map((color: Item) => (
+                                        <MenuItem
+                                            key={color.name}
+                                            value={color.name}
+                                        >
+                                            {color.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            }
+                        </Box>
+                        <Box>
+                            <Typography
+                                component="div"
+                                display="inline-block"
+                                width={150}
+                            >
+                                Background Color:
+                            </Typography>
+                            {
+                                <Select
+                                    style={{
+                                        paddingLeft: 10,
+                                        marginLeft: 20,
+                                        minWidth: 150,
+                                    }}
+                                    variant="filled"
+                                    id="select-background-color"
+                                    value={
+                                        selectedComponent.styles.backgroundColor
+                                    }
+                                    label="Background Color"
+                                    name="backgroundColor"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value="">None</MenuItem>
+                                    {theme.colors?.map((color: Item) => (
+                                        <MenuItem
+                                            key={color.name}
+                                            value={color.name}
+                                        >
+                                            {color.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            }
+                        </Box>
+                        <Box>
+                            <Typography
+                                component="div"
+                                display="inline-block"
+                                width={150}
+                            >
+                                Border Color:
+                            </Typography>
+                            {
+                                <Select
+                                    style={{
+                                        paddingLeft: 10,
+                                        marginLeft: 20,
+                                        minWidth: 150,
+                                    }}
+                                    variant="filled"
+                                    id="select-border-color"
+                                    value={selectedComponent.styles.borderColor}
+                                    label="Border Color"
+                                    name="borderColor"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value="">None</MenuItem>
+                                    {theme.colors?.map((color: Item) => (
+                                        <MenuItem
+                                            key={color.name}
+                                            value={color.name}
+                                        >
+                                            {color.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            }
+                        </Box>
+                        <Box>
+                            <Typography
+                                component="div"
+                                display="inline-block"
+                                width={150}
+                            >
+                                Border Radius:
+                            </Typography>
+                            {
+                                <Select
+                                    style={{
+                                        paddingLeft: 10,
+                                        marginLeft: 20,
+                                        minWidth: 150,
+                                    }}
+                                    variant="filled"
+                                    id="select-border-radius"
+                                    value={
+                                        selectedComponent.styles.borderRadius
+                                    }
+                                    label="Border Radius"
+                                    name="borderRadius"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value="">None</MenuItem>
+                                    {theme.radiusList?.map((radius: Item) => (
+                                        <MenuItem
+                                            key={radius.name}
+                                            value={radius.name}
+                                        >
+                                            {radius.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            }
+                        </Box>
+                        <Box>
+                            <Typography
+                                component="div"
+                                display="inline-block"
+                                width={150}
+                            >
+                                Padding X:
+                            </Typography>
+                            {
+                                <Select
+                                    style={{
+                                        paddingLeft: 10,
+                                        marginLeft: 20,
+                                        minWidth: 150,
+                                    }}
+                                    variant="filled"
+                                    id="select-border-paddingX"
+                                    value={selectedComponent.styles.paddingX}
+                                    label="Border Padding X"
+                                    name="paddingX"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value="">None</MenuItem>
+                                    {theme.spacingList?.map((spacing: Item) => (
+                                        <MenuItem
+                                            key={spacing.name}
+                                            value={spacing.name}
+                                        >
+                                            {spacing.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            }
+                        </Box>
+                        <Box>
+                            <Typography
+                                component="div"
+                                display="inline-block"
+                                width={150}
+                            >
+                                Padding Y:
+                            </Typography>
+                            {
+                                <Select
+                                    style={{
+                                        paddingLeft: 10,
+                                        marginLeft: 20,
+                                        minWidth: 150,
+                                    }}
+                                    variant="filled"
+                                    id="select-border-paddingY"
+                                    value={selectedComponent.styles.paddingY}
+                                    label="Border Padding Y"
+                                    name="paddingY"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value="">None</MenuItem>
+                                    {theme.spacingList?.map((spacing: Item) => (
+                                        <MenuItem
+                                            key={spacing.name}
+                                            value={spacing.name}
+                                        >
+                                            {spacing.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            }
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={5}>
+                    <StyledComponentViewer />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default StyledComponentContainer;
--- client/src/components/StyledComponentViewer.tsx
+++ client/src/components/StyledComponentViewer.tsx
@@ -1,9 +1,217 @@
-import React from 'react'
+import {
+    Box,
+    Button,
+    Checkbox,
+    FormControlLabel,
+    MenuItem,
+    Radio,
+    RadioGroup,
+    Select,
+    TextField,
+} from '@mui/material';
+import { useSelector } from 'react-redux';
+import { ComponentInitialStateType } from '../redux/slices/component';
+import { CSSProperties, useState } from 'react';
+import { Item } from './InputList';
 
-const StyledComponentViewer = () => {
-  return (
-    <div>StyledComponentViewer</div>
-  )
+interface StyledComponentProps {
+    type: string;
+    textColor: string;
+    backgroundColor: string;
+    borderColor: string;
+    borderRadius: string;
+    paddingX: string;
+    paddingY: string;
 }
 
-export default StyledComponentViewer
+const StyledComponent = (props: StyledComponentProps) => {
+    const theme: any = useSelector((state: any) => state.theme);
+    const [selectValue, setSelectValue] = useState('option-1');
+    const [inputValue, setInputValue] = useState('Divyansh Thakur');
+
+    const getValueFromStyleName = (
+        type: 'color' | 'radius' | 'spacing',
+        styleName: string,
+    ) => {
+        switch (type) {
+            case 'color':
+                return theme.colors?.find(
+                    (color: Item) => color.name === styleName,
+                )?.value;
+            case 'radius':
+                const radiusSize = theme.radiusList?.find(
+                    (radius: Item) => radius.name === styleName,
+                )?.value;
+                return radiusSize ? radiusSize + 'px' : '0px';
+            case 'spacing':
+                const spacingSize = theme.spacingList?.find(
+                    (spacing: Item) => spacing.name === styleName,
+                )?.value;
+                return spacingSize ? spacingSize + 'px' : '0px';
+            default:
+        }
+        return '';
+    };
+
+    const commonCustomStyles: CSSProperties = {
+        border: '1px solid black',
+        color: getValueFromStyleName('color', props.textColor),
+        backgroundColor: getValueFromStyleName('color', props.backgroundColor),
+        borderColor: getValueFromStyleName('color', props.borderColor),
+        borderRadius: getValueFromStyleName('radius', props.borderRadius),
+        paddingTop: getValueFromStyleName('spacing', props.paddingY),
+        paddingBottom: getValueFromStyleName('spacing', props.paddingY),
+        paddingLeft: getValueFromStyleName('spacing', props.paddingX),
+        paddingRight: getValueFromStyleName('spacing', props.paddingX),
+    };
+
+    switch (props.type) {
+        case 'button':
+            return (
+                <Button
+                    variant="contained"
+                    size="large"
+                    style={{
+                        cursor: 'pointer',
+                        ...commonCustomStyles,
+                    }}
+                >
+                    Continue
+                </Button>
+            );
+        case 'input-text':
+            return (
+                <TextField
+                    className="input-custom"
+                    placeholder="Text Input Field"
+                    value={inputValue}
+                    onChange={(e: any) => setInputValue(e.target.value)}
+                    inputProps={{
+                        style: {
+                            color: getValueFromStyleName(
+                                'color',
+                                props.textColor,
+                            ),
+                            border: '1px solid black',
+                            borderColor: getValueFromStyleName(
+                                'color',
+                                props.borderColor,
+                            ),
+                            borderRadius: getValueFromStyleName(
+                                'radius',
+                                props.borderRadius,
+                            ),
+                            backgroundColor: getValueFromStyleName(
+                                'color',
+                                props.backgroundColor,
+                            ),
+                            paddingTop: getValueFromStyleName(
+                                'spacing',
+                                props.paddingY,
+                            ),
+                            paddingBottom: getValueFromStyleName(
+                                'spacing',
+                                props.paddingY,
+                            ),
+                            paddingLeft: getValueFromStyleName(
+                                'spacing',
+                                props.paddingX,
+                            ),
+                            paddingRight: getValueFromStyleName(
+                                'spacing',
+                                props.paddingX,
+                            ),
+                        },
+                    }}
+                />
+            );
+        case 'radio':
+            return (
+                <RadioGroup
+                    defaultValue="Default"
+                    name="radio-buttons-group"
+                    style={{ display: 'flex', flexDirection: 'row', gap: 8 }}
+                >
+                    <FormControlLabel
+                        value="Default"
+                        control={<Radio />}
+                        label="Default"
+                        style={commonCustomStyles}
+                    />
+                    <FormControlLabel
+                        value="Other Value"
+                        control={<Radio />}
+                        label="Other Value"
+                        style={commonCustomStyles}
+                    />
+                </RadioGroup>
+            );
+        case 'checkbox':
+            return (
+                <Box display="flex" gap={2}>
+                    <FormControlLabel
+                        control={<Checkbox defaultChecked />}
+                        label="Do you liked it?"
+                        style={commonCustomStyles}
+                    />
+                    <FormControlLabel
+                        style={commonCustomStyles}
+                        control={<Checkbox />}
+                        label="Subscribe me"
+                    />
+                </Box>
+            );
+        case 'select':
+            return (
+                <Select
+                    variant="standard"
+                    value={selectValue}
+                    onChange={(e) => setSelectValue(e.target.value)}
+                    label="Select"
+                    style={{
+                        minWidth: 100,
+                        ...commonCustomStyles,
+                    }}
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value="option-1">Option 1</MenuItem>
+                    <MenuItem value="option-2">Option 2</MenuItem>
+                    <MenuItem value="option-3">Option 3</MenuItem>
+                </Select>
+            );
+        default:
+    }
+    return <></>;
+};
+
+const StyledComponentViewer = () => {
+    const { selectedComponent }: ComponentInitialStateType = useSelector(
+        (state: any) => state.component,
+    );
+
+    return (
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            border="1px solid black"
+            borderRadius={4}
+            padding={2}
+            height="100%"
+        >
+            <StyledComponent
+                type={selectedComponent.type}
+                textColor={selectedComponent.styles.textColor}
+                backgroundColor={selectedComponent.styles.backgroundColor}
+                borderColor={selectedComponent.styles.borderColor}
+                borderRadius={selectedComponent.styles.borderRadius}
+                paddingX={selectedComponent.styles.paddingX}
+                paddingY={selectedComponent.styles.paddingY}
+            />
+        </Box>
+    );
+};
+
+export default StyledComponentViewer;
