0e5e73d8f345000e855faa8ced385c55c6e6aed6
Divyansh Singh Thakur <divyansh2140@gmail.com>
2024-04-22 20:15:34 +0530

    Radius component added

--- client/src/components/RadiusThemeEditor.tsx
+++ client/src/components/RadiusThemeEditor.tsx
@@ -1,8 +1,99 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Box, Container, IconButton } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import InputList, { Item } from './InputList';
+import {
+    useGetThemeByProjectIdQuery,
+    useUpdateThemeByProjectIdMutation,
+} from '../redux/api/theme';
+import { setTheme } from '../redux/slices/theme';
 
 const RadiusThemeEditor = () => {
-  return (
-    <div>RadiusThemeEditor</div>
-  )
-}
+    const dispatch = useDispatch();
+    const [updateTheme] = useUpdateThemeByProjectIdMutation();
+    const projectData = useSelector((state: any) => state.selectedProject);
+    const radiusList: Item[] = useSelector(
+        (state: any) => state.theme.radiusList,
+    );
+    const { data } = useGetThemeByProjectIdQuery(projectData._id, {
+        skip: projectData._id.length === 0,
+    });
+    useEffect(() => {
+        if (data?.colors) dispatch(setTheme({ colors: data.colors }));
+        if (data?.radiusList)
+            dispatch(setTheme({ radiusList: data.radiusList }));
+        if (data?.spacingList)
+            dispatch(setTheme({ spacingList: data.spacingList }));
+    }, [data, dispatch]);
 
-export default RadiusThemeEditor
+    const handleChange = (item: Item) => {
+        const updatedRadiusList = radiusList.map((radius: Item) => {
+            if (radius._id === item._id) {
+                return item;
+            }
+            return radius;
+        });
+        dispatch(setTheme({ radiusList: updatedRadiusList }));
+    };
+
+    const handleDelete = async (item: Item) => {
+        await updateTheme({
+            projectId: projectData._id,
+            radiusList: radiusList.filter(
+                (radius: Item, index: number) =>
+                    index <= 1 || radius._id !== item._id,
+            ),
+        });
+    };
+
+    const roundToEven = (num: number) => {
+        if (num % 2 === 0) return num;
+        return num + 1;
+    };
+
+    const handleAddRadius = async () => {
+        const item: any = {
+            name: `${radiusList.length - 6}xl`,
+            value: `${roundToEven(
+                Math.round(+radiusList[radiusList.length - 1].value * 1.5),
+            )}`,
+        };
+        await updateTheme({
+            projectId: projectData._id,
+            radiusList: [...radiusList, item],
+        });
+    };
+
+    return (
+        <Container
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                paddingTop: 20,
+                paddingBottom: 20,
+            }}
+        >
+            <Box>
+                <InputList
+                    variableNameLabel="Variable Name"
+                    variableValueLabel="Size (Px)"
+                    items={radiusList}
+                    fixedItemCount={7}
+                    onChange={handleChange}
+                    onDelete={handleDelete}
+                />
+                <IconButton
+                    aria-label="add-radius"
+                    color="primary"
+                    onClick={handleAddRadius}
+                    size="large"
+                >
+                    <AddCircleIcon />
+                </IconButton>
+            </Box>
+        </Container>
+    );
+};
+
+export default RadiusThemeEditor;
